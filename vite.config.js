963de991ef113acdf69100e15da4f159ae82c7fc
camelce <camelc.camelc@example.com>
2024-12-05 16:30:48 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Camel-C/', // ضع اسم المستودع هنا
+  base: '/c/', // ضع اسم المستودع هنا
 });
